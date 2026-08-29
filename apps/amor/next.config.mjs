7ac55706be4cf16b7613ad802e790fc748997d6e
hyroxseouldev <confucious12@naver.com>
2026-08-29 21:29:26 +0900

    버그 수정

--- apps/amor/next.config.mjs
+++ apps/amor/next.config.mjs
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sburksnwmywzytpctjws.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
   async redirects() {
     return [
       {
-        source: '/admin',
-        destination: 'https://clyrtraining.vercel.app/t/amor/tenant/login',
+        source: "/admin",
+        destination: "https://clyrtraining.vercel.app/t/amor/tenant/login",
         permanent: true,
       },
     ];
